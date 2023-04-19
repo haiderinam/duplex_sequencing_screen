f04026bac66ae81f3b7e5f33993bcdd8f5fe214e
haiderinam <haider.inam1@gmail.com>
2023-04-18 21:02:19 -0400

    added further ROC analyses of BE screens
    ROC analyses and figures for the BE-SM paper added

--- code/plotting/cleanup.R
+++ code/plotting/cleanup.R
@@ -4,7 +4,7 @@ cleanup=theme_bw() +
         panel.grid.major.y = element_blank(),
         panel.background = element_blank(),
         # axis.line = element_line(color = "black"),
-        axis.text = element_text(face="bold",color="black",size="11"),
-        text=element_text(size=11,face="bold"),
-        axis.title=element_text(face="bold",size="11"))
+        # axis.text = element_text(face="bold",color="black",size="11"),
+        axis.text = element_text(color="black"),
+        text=element_text(size=11,face="bold"))
 # Cleanup is a plotting script that I use to remove background and clean up the ggplot theme etc
